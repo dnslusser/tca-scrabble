9b8925948af6349cffd869d6b54526da737632b5
dnslusser <dslusser@madisoncollege.edu>
2022-03-02 18:00:35 -0600

    Got routes working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import { Route, Routes } from 'react-router';
 
 const Home = () => <h2>Home</h2>;
 const SetupGame = () => <h2>Setup Game</h2>;
@@ -9,6 +10,11 @@ const PlayGame = () => <h2>Play Game</h2>;
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='setup' element={<SetupGame />} />
+        <Route path='play' element={<PlayGame />} />
+      </Routes>
       <Button
       variant = "outlined"
       >
