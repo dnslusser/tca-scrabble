4a37dc1fdd841d9773d316a4acc29549f1aafee2
dnslusser <dslusser@madisoncollege.edu>
2022-03-08 14:10:02 -0600

    Updated home page with basic ui

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 
 export const Home = () => {
 
+    const nav = useNavigate();
+
     return (
         <>
-            <h1
-            
-            >
+            <h1>
                 Scrabble
             </h1>
+            <h2>
+                Welcome!
+            </h2>
+            <br></br>
+            <h3>
+                Top word:
+            </h3>
+            <h3>
+                Win %:
+            </h3>
+            <h3>
+
+            </h3>
             <Button
                 variant = "outlined"
                 color= "success"
+                size="large"
+                onClick={() => nav("/playgame")}
             >
             Play
             </Button>
