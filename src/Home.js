352a767a3ca6844c2f78320ee568771e9ec4a374
dnslusser <dslusser@madisoncollege.edu>
2022-03-08 12:59:30 -0600

    Added Roboto font from MUI
    npm install @fontsource/roboto

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+
+export const Home = () => {
+
+    return (
+        <>
+            <h1
+            
+            >
+                Scrabble
+            </h1>
+            <Button
+                variant = "outlined"
+                color= "success"
+            >
+            Play
+            </Button>
+      </>
+    );
+};
