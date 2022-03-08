55d0c1f412a5a3b2da5893cc74ecb674154650b5
dnslusser <dslusser@madisoncollege.edu>
2022-03-08 14:09:48 -0600

    Added navigate to playgame page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from '@mui/material/Button';
-import { Route, Routes } from 'react-router';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './Home';
+import { PlayGame } from './PlayGame';
 
-const Home = () => <h2>Home</h2>;
-const SetupGame = () => <h2>Setup Game</h2>;
-const PlayGame = () => <h2>Play Game</h2>;
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='setup' element={<SetupGame />} />
-        <Route path='play' element={<PlayGame />} />
+        <Route path='playgame' element={<PlayGame />} />
       </Routes>
-      <Button
-      variant = "outlined"
-      >
-        Play
-      </Button>
-      <Home/>
-      <SetupGame/>
-      <PlayGame/>
     </div>
 
   );
