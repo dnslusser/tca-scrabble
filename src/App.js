95430c0886b4f9664fd71ddfdb9dbf83a6b12fa6
dnslusser <dslusser@madisoncollege.edu>
2022-03-09 14:46:08 -0600

    tried to get theme working
    not working yet :(

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './Home';
 import { PlayGame } from './PlayGame';
 import { Waiting } from './Waiting';
+import { ThemeProvider } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+import { createTheme } from '@mui/system';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[500],
+    },
+  },
+});
 
 function App() {
   return (
+    <>
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='playgame' element={<PlayGame />} />
-        <Route path='waiting' element={<Waiting />}v />
+        <Route path='waiting' element={<Waiting />} />
       </Routes>
-    </div>
-
-  );
+    </div><ThemeProvider theme={theme}></ThemeProvider>
+    </>
+  )
+  
 }
-
 export default App;
