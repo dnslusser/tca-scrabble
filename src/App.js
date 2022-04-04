5adc9860e1be8fba2abf37d1b5f87c9aaae7b25e
dnslusser <dslusser@madisoncollege.edu>
2022-04-04 11:27:38 -0500

    Added scorepage, specialsquares, win and lose pages

--- src/App.js
+++ src/App.js
@@ -3,28 +3,27 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './Home';
 import { PlayGame } from './PlayGame';
 import { Waiting } from './Waiting';
-import { ThemeProvider } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-import { createTheme } from '@mui/system';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: red[500],
-    },
-  },
-});
+import { ScorePage } from './scorePage';
+import { SpecialSquare } from './SpecialSquare';
+import { Win } from './Win';
+import { Lose } from './Lose';
 
 function App() {
+
   return (
     <>
+
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='playgame' element={<PlayGame />} />
         <Route path='waiting' element={<Waiting />} />
+        <Route path='scorePage' element={<ScorePage />} />
+        <Route path='SpecialSquare' element={<SpecialSquare />} />
+        <Route path='win' element={<Win />} />
+        <Route path='lose' element={<Lose />} />
       </Routes>
-    </div><ThemeProvider theme={theme}></ThemeProvider>
+    </div>
     </>
   )
   
