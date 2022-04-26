bcf5939351661d08ef5654094910723f06448686
dnslusser <dslusser@madisoncollege.edu>
2022-04-26 16:20:07 -0500

    Correcting navigation

--- src/scorePage.js
+++ src/scorePage.js
@@ -2,16 +2,36 @@ import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import { TextField } from '@mui/material';
 
-export const ScorePage = () => {
+export const ScorePage = ({
+    addGameResult
+    
+}) => {
 
     const nav = useNavigate();
+
+    const totalScore =  async (score) => {
+
+    }
+
+    const won = () => {
+        addGameResult(true);
+        nav(-3);
+    }
+
+    const lost = () => {
+        addGameResult(false);
+        nav(-3);
+    }
 
     return (
         <>
             <h2>
                 Game Over!
             </h2>
-            <h2>
+            <h2
+                type="number"
+                id="score"
+            >
                 (Your Score displayed)
             </h2>
             -
@@ -20,28 +40,34 @@ export const ScorePage = () => {
             </h2>
             <h2>
             <TextField
+                id="leftOver"
                 type="number"
                 min="0"
                 required="yes"
+                onChange={(event) =>
+                    event.target.value < 0
+                    ? (event.target.value = 0)
+                    : event.target.value
+                }   
             >
                 
             </TextField>
             </h2>
             =
             <h2>
-                (Total Score display)
+                {/* {totalScore} */}
             </h2>
 
             <Button
                 variant="outlined"
                 color="success"
-                onClick={() => nav("/Win")}
+                onClick={won}
             >
                 I Won!
             </Button>
             <Button
                 variant="outlined"
-                onClick={() => nav("/Lose")}
+                onClick={lost}
             >
                 I Lost
             </Button>
