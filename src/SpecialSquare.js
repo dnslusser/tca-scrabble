abd41cc2f744a06ab81d5b99e9c4492aedd74bd4
dnslusser <dslusser@madisoncollege.edu>
2022-04-26 16:19:43 -0500

    cleaning up page
    probably won't be using this page at all in this app version

--- src/SpecialSquare.js
+++ src/SpecialSquare.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { useNavigate } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
 import InputLabel from '@mui/material/InputLabel';
@@ -11,10 +10,10 @@ import Select from '@mui/material/Select';
 
 export const SpecialSquare = () => {
 
-    const [letter, setLetter] = React.useState('');
+    const [letter, setValue, word] = React.useState('');
 
     const handleChange = (event) => {
-        setLetter(event.target.value);
+        setValue(event.target.value);
     }
 
     const nav = useNavigate();
@@ -47,34 +46,80 @@ export const SpecialSquare = () => {
                         onChange={handleChange}
                     >
                         <MenuItem value={"A"}>A</MenuItem>
-                        <MenuItem>B</MenuItem>
-                        <MenuItem>C</MenuItem>
+                        <MenuItem value={"B"}>B</MenuItem>
+                        <MenuItem value={"C"}>C</MenuItem>
                     </Select>
 
                 </FormControl>
             </Box>
             
-            <p>
-                Triple Letter
-                <Button
-                startIcon={<ArrowDropDownIcon/>}
-                >
-                </Button>
-            </p>
-            <p>
-                Double Word
-                <Button
-                startIcon={<ArrowDropDownIcon/>}
-                >
-                </Button>
-            </p>
-            <p>
-                Triple Letter
-                <Button
-                startIcon={<ArrowDropDownIcon/>}
-                >
-                </Button>
-            </p>
+            <Box
+                sx={{ minWidth: 120}}
+            >
+                <FormControl fullWidth>
+                    <InputLabel
+                        id="tripleLetter"
+                    >
+                        Triple Letter
+                    </InputLabel>
+                    <Select
+                        labelId="tripleLetter-label"
+                        id="tripleLetter-select"
+                        value={letter}
+                        label="Letter"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={"A"}>A</MenuItem>
+                        <MenuItem value={"B"}>B</MenuItem>
+                        <MenuItem value={"C"}>C</MenuItem>
+                    </Select>
+
+                </FormControl>
+            </Box>
+            <Box
+                sx={{ minWidth: 120}}
+            >
+                <FormControl fullWidth>
+                    <InputLabel
+                        id="doubleWord"
+                    >
+                        Double Word
+                    </InputLabel>
+                    <Select
+                        labelId="doubleWord-label"
+                        id="doubleWord-select"
+                        value={word}
+                        label="Word"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={1}>Word 1</MenuItem>
+                        <MenuItem value={2}>Word 2</MenuItem>
+                    </Select>
+
+                </FormControl>
+            </Box>
+            <Box
+                sx={{ minWidth: 120}}
+            >
+                <FormControl fullWidth>
+                    <InputLabel
+                        id="doubleWord"
+                    >
+                        Triple Word
+                    </InputLabel>
+                    <Select
+                        labelId="tripleWord-label"
+                        id="tripleWord-select"
+                        value={word}
+                        label="Word"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={1}>Word 1</MenuItem>
+                        <MenuItem value={2}>Word 2</MenuItem>
+                    </Select>
+
+                </FormControl>
+            </Box>
             <Button
                 variant="outlined"
                 color="success"
