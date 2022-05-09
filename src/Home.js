750dcd7b7118073c7282a3b8fe3654d385f99f9b
dnslusser <dslusser@madisoncollege.edu>
2022-05-09 12:45:36 -0500

    Updated home stats

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { Card, CardContent, Typography } from '@mui/material';
 
+const findTopWord = (results) => (
+    Math.max(
+        ...results.map(x => x.score)
+    )
+);
 
-// const calculateTopWord = () => ();
+const findMinWord = (results) => (
+    Math.min(
+        ...results.map(x => x.score)
+    )
+);
 
 export const Home = ({
     gameResults
@@ -22,10 +32,22 @@ export const Home = ({
         nav("/scorepage")
     };
 
-    const win = gameResults.filter(n => n == true).length;
+    const win = gameResults.filter(n => n.winner).length;
+
+    const topWord = findTopWord(gameResults);
+
+    // const topWord = 0;
+
+    // if (gameResults != 0) {
+    //     topWord = findTopWord(gameResults)
+    // } 
+ 
+    const minWord = findMinWord(gameResults);
+
 
     let calculateWinPercentage = ((win / gameResults.length)*100).toFixed(2);
     calculateWinPercentage = isNaN(calculateWinPercentage) ? 0 : Number(calculateWinPercentage);
+
 
     return (
         <>
@@ -37,11 +59,46 @@ export const Home = ({
 
                 <h2>
                     Welcome!
+                    {Date}
                 </h2>
                 <br></br>
-                <h3
+                {/* <Card
+                    sx={{ minWidth: 275}}
+                    
                 >
-                    Top word: Jukebox
+                    <CardContent>
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                        >
+                            Top Score: {topWord}
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                        >
+                            Lowest Score: {minWord}
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            gutterBottom
+                        >
+                            Total Games Played: {gameResults.length}
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                        >
+                            Win Percentage: {calculateWinPercentage} %
+                        </Typography>
+                    </CardContent>
+                </Card> */}
+                <h3
+                    
+                >
+                    Top score: {topWord}
+                </h3>
+                <h3>
+                    Lowest score: {minWord}
                 </h3>
                 <h3>
                     Total games played: {gameResults.length}
@@ -50,6 +107,7 @@ export const Home = ({
                     Win Percentage: {calculateWinPercentage} %
                 </h3>
                 <Button
+                    
                     variant = "outlined"
                     size="large"
                     onClick={startGame}
